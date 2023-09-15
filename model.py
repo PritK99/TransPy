721c8f26183835a1a4a3aa8125e9bb7a1afe7ea4
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-09-15 14:33:43 +0530

    adding class for positional encoding

--- model.py
+++ model.py
@@ -13,3 +13,33 @@ class InputEmbeddings(nn.Module):
     def forward(self, x):
         return self.embeddings(x)*math.sqrt(self.embedding_dim)
 
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, embedding_dim, seq_len, dropout):
+        super().__init__()
+        self.embedding_dim = embedding_dim
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Creating the positional encoding matrix of shape (seq_len, embedding_dim)
+        pe = torch.zeros(seq_len, embedding_dim)
+
+        # Creating a vector of length (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        denominator = torch.exp(torch.arange(0,seq_len,2).float()*(-math.log(10000.0) / embedding_dim))
+
+        pe[:, ::2] = torch.sin(position*denominator)
+        pe[:, 1::2] = torch.cos(position*denominator)
+
+        # Converting (seq_len, embedding_dim) to (1, seq_len, embedding_dim)
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+    
+    def forward(self, x):
+
+        # Adding embeddings with positional encodings. 
+        # x.shape[1] helps to add positional encodings for the positions that are present in the actual input sequence
+        x = x + (self.pe[:, :x.shape[1],:]).requires_grad_(False)
+        return self.dropout(x)
+
