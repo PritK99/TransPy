1cae195eaef939b90048c827b98c1ac266c55eec
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-09-17 17:52:11 +0530

    Working on tokenizers

--- train.py
+++ train.py
@@ -6,3 +6,13 @@ from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.trainers import WordLevelTrainer
+
+from pathlib import Path
+
+def get_or_build_tokenizer(config, dataset, lang):
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=['[SOS]', '[UNK]', '[EOS]', '[PAD]'], min_frequency = 2)
