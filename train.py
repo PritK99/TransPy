cac67931d86a2fdd3981b49f450a11e217572483
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-09-25 15:54:48 +0530

    Adding code for tokenizer

--- train.py
+++ train.py
@@ -9,6 +9,10 @@ from tokenizers.trainers import WordLevelTrainer
 
 from pathlib import Path
 
+def get_sentences(dataset, lang):
+    for entry in dataset:
+        yield entry['translation'][lang]
+
 def get_or_build_tokenizer(config, dataset, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
 
@@ -16,3 +20,9 @@ def get_or_build_tokenizer(config, dataset, lang):
         tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
         tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(special_tokens=['[SOS]', '[UNK]', '[EOS]', '[PAD]'], min_frequency = 2)
+        tokenizer.train_from_iterator(get_sentences(dataset, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    
+    return tokenizer
