c163663e8409146d4c4448d0d77a89d49f8e6edb
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2023-11-19 01:59:55 +0530

    testing training loop

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from tokenizers.pre_tokenizers import Whitespace
 from dataset import BilingualDataset
 from model import build_transformer
 from config import get_weights_file_path, get_config
+from tqdm import tqdm
 
 def get_all_sentences(dataset, lang):
     """
@@ -127,4 +128,52 @@ def train_model(config):
         model_filename = get_weights_file_path(config, config["preload"])
         print(f"Preloading Model: {model_filename}")
         state = torch.load(model_filename)
-        # Work in Progress
+        initial_epoch = state["epoch"]+1
+        optimizer.load_state_dict(state["optimizer_state_dict"])
+        global_step = state["global_step"]
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=src_tokenizer.token_to_id("[PAD]"), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch, config["num_epochs"]):
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing epoch: {epoch:02d}")
+
+        for batch in batch_iterator:
+            encoder_input = batch["encoder_input"].to(device)
+            decoder_input = batch["decoder_input"].to(device)
+            encoder_mask = batch["encoder_mask"].to(device)
+            decoder_mask = batch["decoder_mask"].to(device)
+
+        encoder_output = model.encode(encoder_input, encoder_mask)
+        decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)
+        proj_output = model.project(decoder_output)
+
+        label = batch["label"].to(device)
+
+        loss = loss_fn(proj_output.view(-1, tgt_tokenizer.get_vocab_size()), label.view(-1))
+
+        batch_iterator.set_postfix({f"loss": f"{loss.item():6.3f}"})
+
+        writer.add_scalar("train loss", loss.item(), global_step)
+        writer.flush()
+
+        loss.backward()
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+        global_step+=1
+
+    model_filename = get_weights_file_path(config, f'{epoch:02d}')
+    torch.save(
+        {
+            "epoch": epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "global_step": global_step
+        }, model_filename
+    )
+
+if __name__ == "__main__":
+    config = get_config()
+    train_model(config)
